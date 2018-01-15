96319e48e83fd2b1f5392827dbfe7974845d2f73
Ferenc Erki <erkiferenc@gmail.com>
2018-01-15 06:48:17 +0100

    Convert namespace separators from module to task (fix #1188)

--- lib/Rex/Commands.pm
+++ lib/Rex/Commands.pm
@@ -1035,6 +1035,8 @@ sub needs {
 
   Rex::Logger::debug("need to call tasks from $self");
 
+  $self =~ s/::/:/g;
+
   my @tasks_to_run;
   if($self) {
     @tasks_to_run = $tl->get_all_tasks(qr{^\Q$self\E:[A-Za-z0-9_\-]+$});
